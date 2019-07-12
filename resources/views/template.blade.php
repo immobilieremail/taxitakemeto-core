dca1703427d68646ecbe71513fcbdab70b575a2e
Louis Christner <louis@hopper>
2019-07-12 10:08:22 +0200

    Add template and index view

---
+++ resources/views/template.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf_8">
+        <title>App Name - @yield('title')</title>
+    </head>
+    <body>
+        @yield('content')
+    </body>
+</html>
