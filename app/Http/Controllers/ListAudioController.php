b5afc11397bb2eec929dd4e9eebf28100ed0fb39
Louis Christner <louis.christner@immobilier.email>
2019-07-16 12:30:18 +0200

    Fix routes and list controller

--- app/Http/Controllers/ListAudioController.php
+++ app/Http/Controllers/ListAudioController.php
@@ -7,22 +7,20 @@ use Illuminate\Support\Facades\Storage;
 
 class ListAudioController extends Controller
 {
-    public function index($hash)
+    public function index()
     {
-        // $users = User::where('view' => $hash)->
-        // $path = NULL;
-        // $audios = array();
+        $path = NULL;
+        $audios = array();
 
-        // foreach (Storage::disk('local')->allFiles() as $file) {
-        //     if (strpos($file, 'public/uploads/') !== false) {
-        //         $path = str_replace('public/uploads/', '/storage/uploads/', $file);
-        //         if ($audios !== NULL)
-        //             array_push($audios, $path);
-        //         else
-        //             $audios = $path;
-        //     }
-        // }
-        // return view('list-audio', ['audios' => $audios]);
-        return $hash;
+        foreach (Storage::disk('local')->allFiles() as $file) {
+            if (strpos($file, 'public/uploads/') !== false) {
+                $path = str_replace('public/uploads/', '/storage/uploads/', $file);
+                if ($audios !== NULL)
+                    array_push($audios, $path);
+                else
+                    $audios = $path;
+            }
+        }
+        return view('list-audio', ['audios' => $audios]);
     }
 }
