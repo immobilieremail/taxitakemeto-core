a358e410384df95981954cdd2eee0d2e8a90c475
Louis Christner <louis.christner@immobilier.email>
2019-07-17 17:54:51 +0200

    Add size check on rand_large_nbr

--- app/Http/Controllers/myfunctions/rand_nbr.php
+++ app/Http/Controllers/myfunctions/rand_nbr.php
@@ -6,6 +6,8 @@ function rand_large_nbr()
 
     for ($i = 1; $result < 99999999999999999; $i = $i * 10)
         $result += rand(0, 9) * $i;
+    while ($result > 18446744073709551615)
+        $result /= 10;
     return $result;
 }
 
