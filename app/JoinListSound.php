6b20bfeba80286d17d42f8beca1c37178fa52a33
Louis Christner <louis.christner@immobilier.email>
2019-07-17 11:00:24 +0200

    Add addToDB functions
    Add static function in models : insert new Element in database

--- app/JoinListSound.php
+++ app/JoinListSound.php
@@ -6,5 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class JoinListSound extends Model
 {
-    //
+    public static function addToDB($sound_nbr, $soundlist_nbr)
+    {
+        try {
+            $joinlstsnd = new JoinListSound;
+
+            $joinlstsnd->id_list = $soundlist_nbr;
+            $joinlstsnd->id_sound = $sound_nbr;
+            $joinlstsnd->save();
+            return true;
+        } catch (\Exception $e) {
+            return $e;
+        }
+    }
 }
--- app/Sound.php
+++ app/Sound.php
@@ -4,7 +4,19 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class SoundModel extends Model
+class Sound extends Model
 {
-    //
+    public static function addToDB($id, $filename)
+    {
+        try {
+            $sound = new Sound;
+
+            $sound->id = $id;
+            $sound->path = 'storage/uploads/' . $filename;
+            $sound->save();
+            return true;
+        } catch (\Exception $e) {
+            return $e;
+        }
+    }
 }
