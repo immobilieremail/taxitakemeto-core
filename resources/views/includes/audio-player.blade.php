a3393d4ac93aba1b1223ec117e18598a1c7c46fb
Louis Christner <louis.christner@immobilier.email>
2019-07-17 15:47:37 +0200

    Update partial view audio-player

--- resources/views/includes/audio-player.blade.php
+++ resources/views/includes/audio-player.blade.php
@@ -1,5 +1,18 @@
-<li>
-    <audio controls type="audio">
-        <source src={{ $audio->path }}>
-    </audio>
-</li>
+<ul style="list-style: none;">
+    <?php $isempty = true; ?>
+    @foreach ($lists as $list)
+        @if ($isempty == true)
+            <?php $isempty = false; ?>
+        @endif
+        @foreach ($list as $audio)
+            <li>
+                <audio controls type="audio">
+                    <source src={{ $audio->path }}>
+                </audio>
+            </li>
+        @endforeach
+    @endforeach
+    @if ($isempty == true)
+        It's empty.
+    @endif
+</ul>
--- resources/views/list-audio.blade.php
+++ resources/views/list-audio.blade.php
@@ -6,17 +6,6 @@
 
 @section('content')
     <div>
-        <ul style="list-style: none;">
-            <?php $isempty = true; ?>
-            @foreach ($lists as $list)
-                @if ($isempty == true)
-                    <?php $isempty = false; ?>
-                @endif
-                @each('includes.audio-player', $list, 'audio')
-            @endforeach
-            @if ($isempty == true)
-                It's empty.
-            @endif
-        </ul>
+        @include('includes.audio-player', ['lists' => $lists])
     </div>
 @endsection
