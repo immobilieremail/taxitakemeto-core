70f09c1522967d5403102cebcfd094b6d8835f1f
Louis Christner <louis.christner@immobilier.email>
2019-07-17 11:02:59 +0200

    Add getNbr functions in Edit + View models
    getEditNbr returns id_view from a id_edit given as parameter

--- app/Edit.php
+++ app/Edit.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Edit extends Model
 {
-    //
+    public static function getViewNbr($edit_nbr)
+    {
+        $view_nbr = 0;
+        $edits = Edit::all()->where('id_edit', $edit_nbr);
+
+        foreach ($edits as $edit)
+            $view_nbr = $edit->id_view;
+        return $view_nbr;
+    }
 }
--- app/View.php
+++ app/View.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class View extends Model
 {
-    //
+    public static function getSoundListNbr($view_nbr)
+    {
+        $soundlist_nbr = 0;
+        $views = View::all()->where('id_view', $view_nbr);
+
+        foreach ($views as $views)
+            $soundlist_nbr = $views->id_list;
+        return $soundlist_nbr;
+    }
 }
