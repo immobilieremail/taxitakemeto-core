f7a7b66fa5a8f32a26e6e2b340824086ce9a006d
Louis Christner <louis.christner@immobilier.email>
2019-07-17 12:22:00 +0200

    Add getElement to Edit + View
    getEdit returns Edit::all based on id_edit given as parameter

--- app/Edit.php
+++ app/Edit.php
@@ -6,10 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Edit extends Model
 {
+    public static function getEdits($edit_nbr)
+    {
+        $edits = Edit::all()->where('id_edit', $edit_nbr);
+
+        return $edits;
+    }
+
     public static function getViewNbr($edit_nbr)
     {
         $view_nbr = 0;
-        $edits = Edit::all()->where('id_edit', $edit_nbr);
+        $edits = Edit::getEdits($edit_nbr);
 
         foreach ($edits as $edit)
             $view_nbr = $edit->id_view;
--- app/View.php
+++ app/View.php
@@ -6,10 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class View extends Model
 {
+    public static function getViews($view_nbr)
+    {
+        $views = View::all()->where('id_view', $view_nbr);
+
+        return $views;
+    }
+
     public static function getSoundListNbr($view_nbr)
     {
         $soundlist_nbr = 0;
-        $views = View::all()->where('id_view', $view_nbr);
+        $views = View::getViews($view_nbr);
 
         foreach ($views as $views)
             $soundlist_nbr = $views->id_list;
