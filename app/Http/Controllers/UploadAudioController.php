c8874b8c4749ba18d15fd76e0e3c2a13d76999fc
Louis Christner <louis.christner@immobilier.email>
2019-07-17 12:23:38 +0200

    Check if upload-audio url exists + upload file mime type

--- app/Http/Controllers/UploadAudioController.php
+++ app/Http/Controllers/UploadAudioController.php
@@ -6,7 +6,8 @@ use App\Edit,
     App\View,
     App\Sound,
     App\JoinListSound,
-    App\Http\Requests\AudiosRequest;
+    App\Http\Requests\AudiosRequest,
+    Illuminate\Http\Testing\MimeType;
 
 require_once __DIR__ . "/myfunctions/rand_nbr.php";
 
@@ -20,7 +21,7 @@ class UploadAudioController extends Controller
     private function storeLocally(AudiosRequest $request, $random_nbr)
     {
         $file = $request->file('audio');
-        $extension = $request->file('audio')->getClientOriginalExtension();
+        $extension = $request->file('audio')->extension();
         $filename =  $random_nbr . '.' . $extension;
         $file->move('storage/uploads', $filename);
         return ($filename);
@@ -32,6 +33,11 @@ class UploadAudioController extends Controller
         $success_view = view('upload-audio', ['validation_msg' => 'File has been successfully uploaded.', 'nbr' => $suisse_nbr]);
 
         if ($request->has('audio')) {
+            $extension = $request->file('audio')->extension();
+            $mime = MimeType::get($extension);
+            if (strpos($mime, 'audio') === false)
+                return $failed_view;
+
             $random_nbr = rand_large_nbr();
 
             $filename = $this->storeLocally($request, $random_nbr);
@@ -57,6 +63,13 @@ class UploadAudioController extends Controller
 
     public function show($suisse_nbr)
     {
+        $count = 0;
+        $edits = Edit::getEdits($suisse_nbr);
+
+        foreach ($edits as $edit)
+            $count += 1;
+        if ($count !== 1)
+            return view('404');
         return view('upload-audio', ['validation_msg' => '', 'nbr' => $suisse_nbr]);
     }
 }
