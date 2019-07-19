eac2a24d3ffb4b467af2827c35caf20c7426d079
Louis Christner <louis.christner@immobilier.email>
2019-07-19 14:56:49 +0200

    Update rand_large_nbr function

--- app/Http/Controllers/myfunctions/rand_nbr.php
+++ app/Http/Controllers/myfunctions/rand_nbr.php
@@ -4,10 +4,7 @@ function rand_large_nbr()
 {
     $result = 0;
 
-    for ($i = 1; $result < 99999999999999999; $i = $i * 10)
-        $result += rand(0, 9) * $i;
-    while ($result > 18446744073709551615)
-        $result /= 10;
+    $result = random_int(0, PHP_INT_MAX);
     return $result;
 }
 
