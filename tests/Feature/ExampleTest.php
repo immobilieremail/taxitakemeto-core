62ac7c0ab27771ee84a246f233322b669229d4ad
Louis Christner <louis.christner@immobilier.email>
2019-07-19 18:16:16 +0200

    Add tests
    Add tests on basic routes and database manipulations

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -2,11 +2,19 @@
 
 namespace Tests\Feature;
 
-use Tests\TestCase;
-use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\Edit,
+    App\View,
+    App\Sound,
+    App\SoundList,
+    App\JoinListSound,
+    Tests\TestCase,
+    Illuminate\Foundation\Testing\RefreshDatabase;
 
 class ExampleTest extends TestCase
 {
+
+    use RefreshDatabase;
+
     /**
      * A basic test example.
      *
@@ -15,6 +23,88 @@ class ExampleTest extends TestCase
     public function testBasicTest()
     {
         $response = $this->get('/');
+
+        $response->assertViewIs('index');
+    }
+
+    public function testCreateList()
+    {
+        $gen = $this->post('/');
+        $edit = Edit::all()->first();
+        $match = [
+            'id_edit' => $edit->id_edit,
+            'id_view' => $edit->id_view,
+        ];
+
+        $this->assertDatabaseHas('edits', $match);
+    }
+
+    public function testAccessUpload()
+    {
+        $gen = $this->post('/');
+        $edit = Edit::all()->first();
+        $response = $this->get("/upload-audio/$edit->id_edit");
+
+        $response->assertViewIs('upload-audio');
+    }
+
+    public function testAccessView()
+    {
+        $gen = $this->post('/');
+        $edit = Edit::all()->first();
+        $response = $this->get("/list-audio/$edit->id_view");
+
+        $response->assertViewIs('list-audio');
+    }
+
+    public function testAccessNonExistantUpload()
+    {
+        $response = $this->get('/upload-audio/-gef�z6816#�1heya');
+
+        $response->assertViewIs('404');
+    }
+
+    public function testAccessNonExistantList()
+    {
+        $response = $this->get('/list-audio/-gef�z6816#�1heya');
+
+        $response->assertViewIs('404');
+    }
+
+    public function testDeleteSound()
+    {
+        $gen = $this->post('/');
+        $edit = Edit::all()->first();
+        $view = View::all()->first();
+        $match = [
+            'id' => 123456789,
+            'path' => '/storage/uploads/123456789.mp3',
+        ];
+
+        $sound = Sound::create($match);
+
+        $response = $this->delete("/upload-audio/$edit->id_edit/123456789");
+
+        $this->assertDatabaseMissing('sounds', $match);
+    }
+
+    public function testDeleteNonExistantSound()
+    {
+        $gen = $this->post('/');
+        $edit = Edit::all()->first();
+        $response = $this->delete("/upload-audio/$edit->id_edit/-gef�z6816#�1hey");
+
+        $response->assertStatus(302);
+    }
+
+    public function testCreateAndDeleteList()
+    {
+        $gen = $this->post('/');
+        $edit = Edit::all()->first();
+        $list = SoundList::all()->first();
+        $destroy = SoundList::findOrFail($list->id)->delete();
+
+        $response = $this->get("/upload-audio/$edit->id_edit");
 
         $response->assertStatus(200);
     }
