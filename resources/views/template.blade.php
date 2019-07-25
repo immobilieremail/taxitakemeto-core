15c394230ce8346f91f0ca57dd17e3fe9e51b060
Louis Christner <louis.christner@immobilier.email>
2019-07-25 16:53:30 +0200

    Hotfix last step_2_test pull
    Remove <<<<<HEAD<<<<<

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -5,18 +5,12 @@
         <title>@lang('template_message.title') - @yield('title')</title>
     </head>
     <body>
-<<<<<<< HEAD
-<<<<<<< Updated upstream
-=======
         <div>
             <a href="/">@lang('template_message.go_to_index')</a><br>
         </div>
->>>>>>> Stashed changes
-=======
         <div>
             <a href="/">Go to index</a><br>
         </div>
->>>>>>> 8e383b05347d20ff4d5e920e20543ec58c3cfd99
         @yield('content')
     </body>
 </html>
