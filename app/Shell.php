4484f457dbc68318cabb132da02a8dfe954b695c
Louis Christner <louis.christner@immobilier.email>
2019-07-31 17:10:12 +0200

    Fix Shell Model

--- app/Shell.php
+++ app/Shell.php
@@ -7,10 +7,10 @@ use App\SwissObject;
 class Shell extends SwissObject
 {
     public function audioListViewFacets() {
-        return AudioListViewFacet::where('id_shell', $shell_id)->get()->toArray();
+        return AudioListViewFacet::where('id_shell', $this->shell_id)->get()->toArray();
     }
 
     public function audioListEditFacets() {
-        return AudioListEditFacet::where('id_shell', $shell_id)->get()->toArray();
+        return AudioListEditFacet::where('id_shell', $this->shell_id)->get()->toArray();
     }
 }
