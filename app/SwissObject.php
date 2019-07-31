cbb10c76ae96403768e4b5f0ef254e058e20465f
Louis Christner <louis.christner@immobilier.email>
2019-07-31 17:07:14 +0200

    Make SwissObject findable

--- app/SwissObject.php
+++ app/SwissObject.php
@@ -7,6 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class SwissObject extends Model
 {
     public $incrementing = false;
+    protected $primaryKey = 'swiss_number';
+    protected $keyType = 'string';
 
     public function __construct() {
         $this->swiss_number = base64_encode(gmp_export(gmp_random_bits(128)));
