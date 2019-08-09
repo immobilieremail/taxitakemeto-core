041b8b075a444de29f106b913152f6de81b18cc4
jeFFFairson <jft@tisseur-de-toile.fr>
2019-08-09 15:38:32 +0200

    Update index blade

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,4 +7,19 @@
 @section('content')
     Content
     @lang('auth.failed')
+
+    <div class="col">
+        <div class="btn-group dropup">
+            <button type="button" class="btn btn-secondary btn-sm dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                {{ __('Langue') }}
+            </button>
+            <div class="dropdown-menu">
+                @foreach(config('app.locales') as $locale)
+                    <a class="dropdown-item @if($locale == session('locale')) active @endif" href="{{ route('language', $locale) }}">
+                        {{ $locale }}
+                    </a>
+                @endforeach
+            </div>
+        </div>
+    </div>
 @endsection
