e6e958520994051345e7b59228ca98ab68fca2b3
jeFFFairson <jft@tisseur-de-toile.fr>
2019-08-09 15:38:07 +0200

    Index controller

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -2,21 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\View\View;
 use Illuminate\Http\Request;
+use Illuminate\Http\RedirectResponse;
 
 class IndexController extends Controller
 {
-    public function index()
+    public function index(): View
     {
-
-        app()->setLocale(session()->get('locale'));
         return view('index');
     }
 
-    public function language(String $locale): Request
+    public function language(String $locale): RedirectResponse
     {
         $locale = in_array ($locale, config ('app.locales')) ? $locale : config('app.fallback_locale');
-        session (['locale' => $locale]);
+        session(['locale' => $locale]);
         return back();
     }
 }
