8dcf46975f4d378be8c950af62797d2c1f1d41c9
Louis Christner <louis.christner@immobilier.email>
2019-09-02 11:18:14 +0200

    WIP : Create AudioController

---
+++ app/Http/Controllers/AudioController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AudioController extends Controller
+{
+    public function index()
+    {
+
+    }
+
+    public function post()
+    {
+
+    }
+
+    public function show()
+    {
+
+    }
+
+    public function update()
+    {
+
+    }
+
+    public function destroy()
+    {
+
+    }
+}
