98945142a26b2246c4b4503d3038ecdc78fcb90f
Louis Christner <louis.christner@immobilier.email>
2019-09-04 17:41:08 +0200

    Create 400 Bad Request error view

---
+++ resources/views/errors/400.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Bad Request</title>
+    </head>
+    <body>
+        <div class="code">
+            400
+        </div>
+        <div class="message" style="padding: 10px;">
+            Bad Request
+        </div>
+    </body>
+</html>
