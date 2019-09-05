335bae0f21ed11c1e329f47cef3113a225505915
Louis Christner <louis.christner@immobilier.email>
2019-09-05 11:40:43 +0200

    Set Audio as Incrementer + add Audio Facets

---
+++ database/migrations/2019_09_05_090319_switch_audio_to_increment.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class SwitchAudioToIncrement extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('audio', function (Blueprint $table) {
+            $table->dropColumn('swiss_number');
+            $table->dropColumn('path');
+        });
+        Schema::table('audio', function (Blueprint $table) {
+            $table->string('path')->nullable();
+            $table->bigIncrements('id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('audio', function (Blueprint $table) {
+            $table->dropColumn('id');
+            $table->dropColumn('path');
+        });
+        Schema::table('audio', function (Blueprint $table) {
+            $table->string('path');
+            $table->string('swiss_number', 24)->primary();
+        });
+    }
+}
---
+++ database/migrations/2019_09_05_090720_create_audio_edit_facets_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAudioEditFacetsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('audio_edit_facets', function (Blueprint $table) {
+            $table->string('swiss_number', 24)->primary();
+            $table->bigInteger('id_audio')->unsigned();
+            $table->timestamps();
+
+            $table->foreign('id_audio')
+                ->references('id')
+                ->on('audio')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('audio_edit_facets');
+    }
+}
