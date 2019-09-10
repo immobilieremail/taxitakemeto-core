120cc9c06c24c43e87befdfe6a8d8ee39d780738
Louis <l.christner@orange.fr>
2019-09-10 16:47:51 +0200

    Add Remove Audio from AudioList

--- routes/api.php
+++ routes/api.php
@@ -16,7 +16,7 @@ use Illuminate\Http\Request;
 
 Route::resource('audiolist', 'AudioListController')->only('store', 'show', 'edit');
 Route::post('audiolist/{audiolist_id}/add_audio', 'AudioListController@add_audio')->name('audiolist.add_audio');
-Route::resource('audio', 'AudioController')->only('store', 'destroy');
+Route::post('audiolist/{audiolist_id}/remove_audio', 'AudioListController@remove_audio')->name('audiolist.remove_audio');
 
 Route::any('/{catchall}', function() {
     return abort(404);
