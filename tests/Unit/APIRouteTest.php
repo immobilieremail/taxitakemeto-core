20bccc2152cf4641c9e237a181804a5f841040c6
Louis <l.christner@orange.fr>
2019-09-16 16:30:56 +0200

    Create tests for Shell
    Test Shell update through QuickCheck

--- tests/Unit/APIRouteTest.php
+++ tests/Unit/APIRouteTest.php
@@ -87,9 +87,8 @@ class APIRouteTest extends TestCase
         $response->assertStatus(404);
     }
 
-    private function generateAudiosJson($audiolist) : Array
+    private function generateAudiosJson($audiolist, $random) : Array
     {
-        $random = rand(0, 10);
         $audio_array["audios"] = [];
 
         for ($i = 0; $i < $random; $i++) {
@@ -109,12 +108,12 @@ class APIRouteTest extends TestCase
     /** @test */
     public function updateAudiolist()
     {
-        $this->limitTo(10)->forAll(Generator\nat())->then(function () {
+        $this->limitTo(10)->forAll(Generator\nat())->then(function ($random) {
             $audiolist = AudioList::create();
             $audiolist_edit = AudioListEditFacet::create(['id_list' => $audiolist->id]);
             $audiolist_view = AudioListViewFacet::create(['id_list' => $audiolist->id]);
 
-            $audio_array = $this->generateAudiosJson($audiolist);
+            $audio_array = $this->generateAudiosJson($audiolist, $random);
             $response = $this->put("/api/audiolist/$audiolist_edit->swiss_number", ["data" => $audio_array]);
             $mapped_audio = array_map(function ($audio) {
                 return [
@@ -211,6 +210,7 @@ class APIRouteTest extends TestCase
         $audio_edit = AudioEditFacet::create(['id_audio' => $audio->id]);
         $audio_view = AudioViewFacet::create(['id_audio' => $audio->id]);
 
+        /** Create empty file */
         $file = "public/storage/converts/$audio->path";
         $handle = fopen($file, 'w');
         fclose($handle);
@@ -258,5 +258,94 @@ class APIRouteTest extends TestCase
 
         $response = $this->get("/api/shell/$shell->swiss_number");
         $response->assertStatus(200);
+    }
+
+    /** @test */
+    public function accessToAudioListThroughShell()
+    {
+        $shell = Shell::create();
+
+        $audiolist = AudioList::create();
+        $audiolist_edit = AudioListEditFacet::create(['id_list' => $audiolist->id]);
+        $audiolist_view = AudioListViewFacet::create(['id_list' => $audiolist->id]);
+
+        $audiolist_edit->shells()->save($shell);
+        $audiolist_view->shells()->save($shell);
+
+        $view_array[] = [
+            "type" => "ocap",
+            "ocapType" => "AudioListView",
+            "url" => "/api/audiolist/$audiolist_view->swiss_number"
+        ];
+        $edit_array[] = [
+            "type" => "ocap",
+            "ocapType" => "AudioListEdit",
+            "url" => "/api/audiolist/$audiolist_edit->swiss_number/edit"
+        ];
+
+        $response = $this->get("/api/shell/$shell->swiss_number");
+        $this->assertEquals(json_encode($view_array),
+            json_encode(json_decode($response->getContent())->contents->audiolists_view));
+        $this->assertEquals(json_encode($edit_array),
+            json_encode(json_decode($response->getContent())->contents->audiolists_edit));
+    }
+
+    private function generateAudioListsJson($shell, $random)
+    {
+        $audiolist_array["audiolists"] = [];
+
+        for ($i = 0; $i < $random; $i++) {
+            $random_facet = random_int(0, 1);
+            $audiolist = AudioList::create();
+            $audiolist_view = AudioListViewFacet::create(['id_list' => $audiolist->id]);
+            $audiolist_edit = AudioListEditFacet::create(['id_list' => $audiolist->id]);
+
+            if ($random_facet == 0) {
+                $shell->audioListViews()->save($audiolist_view);
+                $audiolist_array["audiolists"][] = [
+                    'id' => $audiolist_view->swiss_number,
+                    'type' => 'view'
+                ];
+            } else {
+                $shell->audioListEdits()->save($audiolist_edit);
+                $audiolist_array["audiolists"][] = [
+                    'id' => $audiolist_edit->swiss_number,
+                    'type' => 'edit'
+                ];
+            }
+        }
+        return $audiolist_array;
+    }
+
+    /** @test */
+    public function updateShell()
+    {
+        $this->limitTo(10)->forAll(Generator\nat())->then(function ($random) {
+            $shell = Shell::create();
+
+            $audiolist_array = $this->generateAudioListsJson($shell, $random);
+            $response = $this->put("/api/shell/$shell->swiss_number", ["data" => $audiolist_array]);
+            $mapped_view = array_map(function($audiolist) {
+                if (!empty($audiolist) && $audiolist["type"] == "view")
+                    return [
+                        'type' => 'ocap',
+                        'ocapType' => 'AudioListView',
+                        'url' => '/api/audiolist/' . $audiolist["id"]
+                    ];
+            }, $audiolist_array["audiolists"]);
+            $mapped_edit = array_map(function($audiolist) {
+                if (!empty($audiolist) && $audiolist["type"] == "edit")
+                    return [
+                        'type' => 'ocap',
+                        'ocapType' => 'AudioListEdit',
+                        'url' => '/api/audiolist/' . $audiolist["id"] . '/edit'
+                    ];
+            }, $audiolist_array["audiolists"]);
+            $response->assertStatus(200);
+            $this->assertEquals(json_encode($response->getData()->contents->audiolists_edit),
+                json_encode(array_values(array_filter($mapped_edit))));
+            $this->assertEquals(json_encode($response->getData()->contents->audiolists_view),
+                json_encode(array_values(array_filter($mapped_view))));
+        });
     }
 }
