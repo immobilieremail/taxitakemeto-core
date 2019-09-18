a210c2c37512a89b3d835aa7f03b608ace0be5f6
Louis <l.christner@orange.fr>
2019-09-18 14:18:41 +0200

    Create Rule + Request to send ShellDropbox message

---
+++ app/Rules/DropboxMessageRules.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Rules;
+
+use Illuminate\Contracts\Validation\Rule;
+
+class DropboxMessageRules implements Rule
+{
+    /**
+     * Determine if the validation rule passes.
+     *
+     * @param  string  $attribute
+     * @param  mixed  $value
+     * @return bool
+     */
+    public function passes($attribute, $value)
+    {
+        if (is_array($value)) {
+            foreach ($value as $data) {
+                if (!isset($data["ocapType"], $data["dropbox"], $data["ocap"])
+                    || !is_string($data["ocapType"])
+                    || !is_string($data["dropbox"])
+                    || !is_string($data["ocap"])
+                    || !preg_match('#\w+==#', $data["dropbox"])
+                    || !preg_match('#\w+==#', $data["ocap"]))
+                    return false;
+            }
+            return true;
+        } else
+            return false;
+    }
+
+    /**
+     * Get the validation error message.
+     *
+     * @return string
+     */
+    public function message()
+    {
+        return 'The validation error message.';
+    }
+}
