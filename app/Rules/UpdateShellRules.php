520ed079978324ff70e411b1d0d726e307fa3a60
Louis <l.christner@orange.fr>
2019-09-19 10:41:40 +0200

    Create request + rule for update Shell

---
+++ app/Rules/UpdateShellRules.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Rules;
+
+use Illuminate\Contracts\Validation\Rule;
+
+class UpdateShellRules implements Rule
+{
+    /**
+     * Determine if the validation rule passes.
+     *
+     * @param  string  $attribute
+     * @param  mixed  $value
+     * @return bool
+     */
+    public function passes($attribute, $value)
+    {
+        if (is_array($value)) {
+            if (isset($value["audiolists"]) && is_array($value["audiolists"])) {
+                foreach ($value["audiolists"] as $audiolist) {
+                    if (!isset($audiolist["ocapType"], $audiolist["ocap"])
+                        || !is_string($audiolist["ocapType"])
+                        || !is_string($audiolist["ocap"])
+                        || !preg_match('#[^/]+$#', $audiolist["ocap"], $matches)) {
+                        return false;
+                    }
+                }
+                return true;
+            } else
+                return false;
+        } else
+            return false;
+    }
+
+    /**
+     * Get the validation error message.
+     *
+     * @return string
+     */
+    public function message()
+    {
+        return 'The validation error message.';
+    }
+}
