902879ac0d85008a7d6641f5ba067463bbc27508
Pierre Thierry <pierre@nothos.net>
2019-09-23 14:12:06 +0200

    Add Elm

---
+++ resources/views/elm.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf_8">
+        <title>TTMT</title>
+	<script src="/js/app.js" defer></script>
+    </head>
+    <body>
+    </body>
+</html>
