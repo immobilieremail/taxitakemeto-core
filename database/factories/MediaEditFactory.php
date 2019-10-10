2a64bfced0bbaed2bcdda0d80abed6d81f010115
mohtth <mtoutouhi@gmail.com>
2019-10-10 15:53:44 +0200

    Create factory media facets

---
+++ database/factories/MediaEditFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\MediaEdit;
+use Faker\Generator as Faker;
+
+$factory->define(MediaEdit::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
---
+++ database/factories/MediaViewFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\MediaView;
+use Faker\Generator as Faker;
+
+$factory->define(MediaView::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
