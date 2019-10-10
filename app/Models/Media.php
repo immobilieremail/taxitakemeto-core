556a9959aba538cb7c634e9dff208d34f92b460b
mohtth <mtoutouhi@gmail.com>
2019-10-10 15:51:39 +0200

    Create relations media facets

--- app/Models/Media.php
+++ app/Models/Media.php
@@ -2,9 +2,24 @@
 
 namespace App\Models;
 
+use App\Models\MediaView;
+use App\Models\MediaEdit;
+
 use Illuminate\Database\Eloquent\Model;
 
 class Media extends Model
 {
-    //
+    protected $fillable = [
+        'path', 'mimetype'
+    ];
+
+    public function viewFacet()
+    {
+        return $this->morphOne(MediaView::class, 'target');
+    }
+
+    public function editFacet()
+    {
+        return $this->morphOne(MediaEdit::class, 'target');
+    }
 }
