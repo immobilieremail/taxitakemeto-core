399c0c389643f0c693893180001b5a4928ecd2df
mohtth <mtoutouhi@gmail.com>
2019-10-10 15:48:36 +0200

    Create facet migration

---
+++ database/migrations/2019_10_09_131943_facets.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Facets extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('facets', function (Blueprint $table) {
+            $table->string('swiss_number', 24)->primary();
+            $table->bigInteger('target_id')->unsigned()->nullable();
+            $table->string('target_type')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('facets');
+    }
+}
