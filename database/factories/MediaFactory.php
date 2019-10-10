14a04f817b451cad27fd379f923532180d1326f0
mohtth <mtoutouhi@gmail.com>
2019-10-10 16:49:34 +0200

    Create media factory faker

--- database/factories/MediaFactory.php
+++ database/factories/MediaFactory.php
@@ -2,11 +2,22 @@
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
-use App\Media;
+use App\Models\Media;
+use App\Models\MediaEdit;
+use App\Models\MediaView;
 use Faker\Generator as Faker;
 
 $factory->define(Media::class, function (Faker $faker) {
     return [
-        //
+        'path' => $faker->url(),
+        'mimetype' => $faker->mimeType
     ];
+});
+
+$factory->afterCreating(Media::class, function ($media, $faker) {
+    $media->viewFacet()->save(factory(MediaView::class)->make());
+});
+
+$factory->afterCreating(Media::class, function ($media, $faker) {
+    $media->editFacet()->save(factory(MediaEdit::class)->make());
 });
