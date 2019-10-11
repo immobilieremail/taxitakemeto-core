d675c0e1e24b993e985a1e2792970a0851e45b3c
jeFFFairson <jft@tisseur-de-toile.fr>
2019-10-11 14:41:01 +0200

    Rename and update facet migration

--- database/migrations/2019_10_09_131943_create_facet_table.php
+++ database/migrations/2019_10_09_131943_create_facet_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class Facets extends Migration
+class CreateFacetTable extends Migration
 {
     /**
      * Run the migrations.
