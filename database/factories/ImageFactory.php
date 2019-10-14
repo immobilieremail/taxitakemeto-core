4a51249df5855c7f6b81c33e5c46f2ce7e6a41f3
mohtth <mtoutouhi@gmail.com>
2019-10-14 18:12:48 +0200

    Setup media factories

--- database/factories/ImageFactory.php
+++ database/factories/ImageFactory.php
@@ -7,6 +7,16 @@ use Faker\Generator as Faker;
 
 $factory->define(Image::class, function (Faker $faker) {
     return [
-        //
+        'path' => $faker->url(),
+        'mimetype' => $faker->mimeType,
+        'media_type' => "image"
     ];
+});
+
+$factory->afterCreating(Media::class, function ($media, $faker) {
+    $media->viewFacet()->save(factory(MediaView::class)->make());
+});
+
+$factory->afterCreating(Media::class, function ($media, $faker) {
+    $media->editFacet()->save(factory(MediaEdit::class)->make());
 });
