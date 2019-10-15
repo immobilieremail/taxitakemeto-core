402d5e5faea93bad138aaf9b4905efa14af1c4f0
mohtth <mtoutouhi@gmail.com>
2019-10-15 18:23:25 +0200

    Update SwissObject model

--- app/Models/SwissObject.php
+++ app/Models/SwissObject.php
@@ -1,22 +1,36 @@
 <?php
 
-namespace App;
+namespace App\Models;
 
 use App\Extensions\SwissNumber;
 use Illuminate\Database\Eloquent\Model;
 
 abstract class SwissObject extends Model
 {
-    public $incrementing = false;
-    protected $primaryKey = 'swiss_number';
-    protected $keyType = 'string';
-    protected $table = 'facets';
+    /**
+     * Setup to use string primary keys
+     * @var boolean
+     */
+    public      $incrementing   = false;
+    protected   $keyType        = 'string';
 
+    /**
+     * Swiss model have string casted PrimaryKey
+     * @var [type]
+     */
+    protected   $cast           = [
+        'id'    => 'string'
+    ];
+
+    /**
+     * Constructor to affect swiss number @ model creation
+     *
+     * @param array $attributes [description]
+     */
     public function __construct(array $attributes = array())
     {
         parent::__construct($attributes);
 
-        $swiss_number = new SwissNumber;
-        $this->swiss_number = $swiss_number();
+        $this->id = swiss_number();
     }
 }
