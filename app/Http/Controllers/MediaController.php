ae6e0c0f54bcd85d75d51326dd787f1fc482594a
mohtth <mtoutouhi@gmail.com>
2019-10-16 11:29:42 +0200

    Delete useless functions

--- app/Http/Controllers/MediaController.php
+++ app/Http/Controllers/MediaController.php
@@ -8,26 +8,6 @@ use Illuminate\Http\Request;
 class MediaController extends Controller
 {
     /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
@@ -45,29 +25,6 @@ class MediaController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function show(Media $media)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Media  $media
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Media $media)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Media  $media
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Media $media)
     {
         //
     }
