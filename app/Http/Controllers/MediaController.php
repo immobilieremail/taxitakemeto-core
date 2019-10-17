2b8c6a48ea4a9a48d3478143c43dab44ec5beeaf
mohtth <mtoutouhi@gmail.com>
2019-10-17 18:19:03 +0200

    Update Store function

--- app/Http/Controllers/MediaController.php
+++ app/Http/Controllers/MediaController.php
@@ -2,8 +2,13 @@
 
 namespace App\Http\Controllers;
 
-use App\Media;
+use App\Models\Media;
 use Illuminate\Http\Request;
+use App\Models\MediaEditFacet;
+use App\Models\MediaViewFacet;
+use App\Jobs\ConvertUploadedAudio;
+use App\Jobs\ConvertUploadedImage;
+use App\Jobs\ConvertUploadedVideo;
 
 class MediaController extends Controller
 {
@@ -15,28 +20,26 @@ class MediaController extends Controller
      */
     public function store(Request $request)
     {
-        //
-    }
+        $media_name = swiss_number() . '.' . $request->file('media')->extension();
+        $path = $request->file('media')->storeAs('', $media_name, 'uploads');
+        $mime_type = $request->file('media')->getMimeType();
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Media  $media
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Media $media)
-    {
-        //
-    }
+        $media_type = [];
+        preg_match('#(^[a-z])\w+#', $mime_type, $media_type);
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Media  $media
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Media $media)
-    {
-        //
+        $media = Media::create(['path'=>$path, 'media_type'=>$media_type[0]]);
+        $media->editFacet()->save(new MediaEditFacet);
+        $media->viewFacet()->save(new MediaViewFacet);
+
+        if ($media_type[0] == 'audio'){
+            $this->dispatch(new ConvertUploadedAudio($media));
+        }
+        elseif ($media_type[0] == 'video') {
+            $this->dispatch(new ConvertUploadedVideo($media));
+        }
+        elseif ($media_type[0] == 'image') {
+            $this->dispatch(new ConvertUploadedImage($media));
+        }
+        return response('Success !', 200);
     }
 }
