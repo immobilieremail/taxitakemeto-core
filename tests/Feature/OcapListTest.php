639b0fb384751f7f196993b7647d6a8a28aec9f8
mohtth <mtoutouhi@gmail.com>
2019-10-21 15:50:14 +0200

    Fix test name

--- tests/Feature/OcapListTest.php
+++ tests/Feature/OcapListTest.php
@@ -6,7 +6,7 @@ use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
-class ListTest extends TestCase
+class OcapListTest extends TestCase
 {
     /**
      * A basic feature test example.
