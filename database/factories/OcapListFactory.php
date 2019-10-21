91c60dd7ac8c3fc5b322271ac192aa718e131dcb
mohtth <mtoutouhi@gmail.com>
2019-10-21 15:48:38 +0200

    Create OcapList resources

---
+++ database/factories/OcapListFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\OcapList;
+use Faker\Generator as Faker;
+
+$factory->define(OcapList::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
