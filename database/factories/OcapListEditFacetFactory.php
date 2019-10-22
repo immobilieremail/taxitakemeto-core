f91959a22f65e6adddae511a7be83beaf6f17a02
mohtth <mtoutouhi@gmail.com>
2019-10-22 12:37:28 +0200

    Create OcapList factories

---
+++ database/factories/OcapListEditFacetFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\OcapListEditFacet;
+use Faker\Generator as Faker;
+
+$factory->define(OcapListEditFacet::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
---
+++ database/factories/OcapListViewFacetFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\OcapListViewFacet;
+use Faker\Generator as Faker;
+
+$factory->define(OcapListViewFacet::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
