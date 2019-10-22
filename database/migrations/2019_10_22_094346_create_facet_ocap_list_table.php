234282441490b5ee6be56d650b8686232222e5e6
Louis Christner <louis.christner@immobilier.email>
2019-10-22 12:07:02 +0200

    Create relationship between OcapLists & Facets

---
+++ database/migrations/2019_10_22_094346_create_facet_ocap_list_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFacetOcapListTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('facet_ocap_list', function (Blueprint $table) {
+            $table->bigInteger('ocap_list_id');
+            $table->string('facet_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('listables');
+    }
+}
