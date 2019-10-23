a7b63a4d5da18db0e7c806fb083ada0686405f28
mohtth <mtoutouhi@gmail.com>
2019-10-23 16:49:55 +0200

    Add bootstrap stylesheet

--- resources/views/elm.blade.php
+++ resources/views/elm.blade.php
@@ -3,6 +3,8 @@
     <head>
         <meta charset="utf-8">
         <title>TTMT</title>
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 	<script src="/js/app.js" defer></script>
     </head>
     <body>
