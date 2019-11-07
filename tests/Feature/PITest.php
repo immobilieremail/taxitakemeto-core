9363ea4d156700e5996bdca7ee9888222ef6bda9
Louis Christner <louis.christner@immobilier.email>
2019-11-07 17:24:16 +0100

    Create view & edit PI tests

--- tests/Feature/PITest.php
+++ tests/Feature/PITest.php
@@ -8,6 +8,8 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 
 use App\Models\OcapList;
 
+use App\Models\PI;
+
 class PITest extends TestCase
 {
     use RefreshDatabase;
@@ -35,5 +37,70 @@ class PITest extends TestCase
                 'ocapType',
                 'url'
             ]);
+    }
+
+    /**
+     * @test
+     *
+     */
+    public function view_pi()
+    {
+        $pi = factory(PI::class)->create();
+        $response = $this->get(route('pi.show', ['obj' => $pi->viewFacet->id]));
+        $response
+            ->assertStatus(200)
+            ->assertJsonCount(3)
+            ->assertJsonStructure([
+                'type',
+                'data' => [
+                    'title',
+                    'description',
+                    'medias' => []
+                ]
+            ]);
+    }
+
+    /**
+     * @test
+     *
+     */
+    public function bad_view_pi()
+    {
+        $response = $this->get(route('pi.show', ['obj' => 'something bad']));
+        $response
+            ->assertStatus(404);
+    }
+
+    /**
+     * @test
+     *
+     */
+    public function edit_pi()
+    {
+        $pi = factory(PI::class)->create();
+        $response = $this->get(route('pi.show', ['obj' => $pi->editFacet->id]));
+        $response
+            ->assertStatus(200)
+            ->assertJsonCount(3)
+            ->assertJsonStructure([
+                'type',
+                'view_facet',
+                'data' => [
+                    'title',
+                    'description',
+                    'medias' => []
+                ]
+            ]);
+    }
+
+    /**
+     * @test
+     *
+     */
+    public function bad_edit_pi()
+    {
+        $response = $this->get(route('pi.show', ['obj' => 'something very bad']));
+        $response
+            ->assertStatus(404);
     }
 }
