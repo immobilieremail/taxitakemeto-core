1758451c98f425a8bf23b7f87a91657894f868b0
Louis Christner <louis.christner@immobilier.email>
2019-11-08 17:22:15 +0100

    Create OverButton module

---
+++ resources/elm/OverButton.elm
@@ -0,0 +1,19 @@
+module OverButton exposing (..)
+
+type OverButton
+  = CarouselPrevButton
+  | CarouselNextButton
+
+
+
+-- CONSTRUCTORS
+
+
+carouselPrevButton : OverButton
+carouselPrevButton =
+    CarouselPrevButton
+
+
+carouselNextButton : OverButton
+carouselNextButton =
+    CarouselNextButton
