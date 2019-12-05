7bf2695f271ccd7dadc5fe6067f8d5417f2d8438
Louis Christner <louis.christner@immobilier.email>
2019-12-05 11:13:47 +0100

    Create PI updateTarget

--- app/Models/PIEditFacet.php
+++ app/Models/PIEditFacet.php
@@ -67,4 +67,36 @@ class PIEditFacet extends Facet
         $this->target->delete();
         $this->delete();
     }
+
+    public function has_update()
+    {
+        return true;
+    }
+
+    public function updateTarget(Request $request)
+    {
+        $allowed = ['title', 'description', 'address'];
+        $new_data = array_intersect_key($request->all(), array_flip($allowed));
+        $tested_data = array_filter($new_data, function ($value, $key) {
+            $tests = [
+                'title' => is_string($value),
+                'description' => is_string($value),
+                'address' => is_string($value)
+            ];
+
+            return $tests[$key];
+        }, ARRAY_FILTER_USE_BOTH);
+
+        $this->target->update($tested_data);
+        if ($request->has('medias') && is_string($request->medias)) {
+            $listFacet = Facet::all()->where('id', getSwissNumberFromUrl($request->medias))->first(); // BAAD
+            if ($listFacet == null) {
+                return false;
+            } else {
+                $this->target->mediaOcapListFacets()->detach();
+                $this->target->mediaOcapListFacets()->save($listFacet);
+            }
+        }
+        return true;
+    }
 }
