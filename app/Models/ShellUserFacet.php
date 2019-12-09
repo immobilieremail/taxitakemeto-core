4421ab85382d55c28472e35a80de5c81645956ec
Louis Christner <louis.christner@immobilier.email>
2019-12-09 15:24:39 +0100

    Add ShellUserFacet construct

--- app/Models/ShellUserFacet.php
+++ app/Models/ShellUserFacet.php
@@ -4,6 +4,13 @@ namespace App\Models;
 
 class ShellUserFacet extends Facet
 {
+    public function __construct()
+    {
+        parent::__construct();
+
+        $this->type = 'App\Models\ShellUserFacet';
+    }
+
     public function target()
     {
         return $this->belongsTo(Shell::class);
