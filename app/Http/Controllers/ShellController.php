65b29f7c63568617b9fb0f8b693149b8ba1347d4
Louis Christner <louis.christner@immobilier.email>
2019-12-10 17:02:58 +0100

    Update ShellController store method

--- app/Http/Controllers/ShellController.php
+++ app/Http/Controllers/ShellController.php
@@ -2,17 +2,33 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Http\Requests\ShellRequest;
+
+use App\Models\Facet;
 
 use App\Models\Shell;
 use App\Models\ShellUserFacet;
+use App\Models\ShellDropboxFacet;
 
 class ShellController extends Controller
 {
-    public function store()
+    public function store(ShellRequest $request)
     {
         $shell = Shell::create();
         $shell->userFacet()->save(new ShellUserFacet);
+        $shell->dropboxFacet()->save(new ShellDropboxFacet);
+
+        $fields = intersectFields(['travels', 'contacts'], $request->all());
+        $relations = [
+            'travels' => $shell->travelOcapListFacets(),
+            'contacts' => $shell->contactOcapListFacets()
+        ];
+
+        foreach ($fields as $i => $field) {
+            $relations[$i]->save(
+                Facet::find(getSwissNumberFromUrl($field))
+            );
+        }
 
         return response()->json([
             'type' => 'ocap',
